35dfb8de2340eba331563ee9a62c08e20be9fdca
= <=>
2020-03-10 17:55:29 +0700

    add file Cricle_svg and Box_chat

--- src/App.js
+++ src/App.js
@@ -2,10 +2,14 @@ import React from 'react';
 import './App.css';
 import Header from './Components/Blocks/Header/Header';
 import Topbar from './Components/Blocks/Header/Topbar';
+import Cricle_Svg from './Components/Blocks/Contacts/Cricle_Svg';
+import Box_chat from './Components/Blocks/Contacts/Box_chat';
 
 function App() {
   return (
     <div className="App">
+      <Cricle_Svg></Cricle_Svg>
+      <Box_chat></Box_chat>
       <Topbar></Topbar>
       <hr></hr>
       <Header></Header>
---
+++ src/Components/Blocks/Contacts/Box_chat.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+
+export default class Box_chat extends Component {
+    render() {
+        return (
+            <div className="box_chat">
+                <div className="icon_chat">
+                    <i class="fas fa-comment-alt"></i>
+                </div>
+                <div className="content_chat">
+                    bạn cần hỗ trợ ? chat ngay
+                </div>
+                
+            </div>
+        )
+    }
+}
